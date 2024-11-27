8c6db2a36d9c32e6c7fc35cb87601cf9bb2f849e
Ahsan <50600624+atebha@users.noreply.github.com>
2024-11-27 11:43:34 -0800

    Add files via upload

---
+++ custom_module.py
@@ -0,0 +1,6 @@
+# Custom Module for import
+
+# Time Travelling Function
+def generate_time_travel_message(year, destination, cost, currency):
+    return f"Prepare for jump! You are travelling to {destination} in the year of {year}. The cost of the trip is {currency}{cost}."
+
