b0d36bc4eef37301af022f42a2663a9a4f00137e
Ahsan <50600624+atebha@users.noreply.github.com>
2025-02-12 23:33:56 -0800

    final version

--- Time Traveler's Toolkit.py
+++ Time Traveler's Toolkit.py
@@ -3,11 +3,30 @@
 import custom_module as cm
 import datetime as dt
 import decimal as decimal
-from random import randint
-from random import choice
+from random import randint, choice
 
+
+# Set current date and time
 date = dt.datetime.today()
 time = dt.datetime.now().time()
 
 # Retrieve current date and time
-print(f"Today's date is {date}. The current {time}.")
+print(f"Today's date is {date.strftime('%Y-%M-$D')}. The current {time.strftime('%H:%M')}.")
+current_year = date.year
+
+# Base cost for Time Travel
+base_cost = decimal.Decimal(1500.00)
+target_year = int(input("Enter the year you wish to visit: "))
+difference = abs(target_year - current_year)
+cost_multiplier = decimal('1.5')
+final_cost = base_cost + (decimal(difference) * cost_multiplier)
+final_cost = final_cost.quantize(decimal('0.01'))
+
+# Randomly select a destination
+destinations = ["Ancient Rome", "Medieval Europe", "The Distant Future", "Jurassic Era", "Renaissance Italy"]
+selected_destination = choice(destinations)
+
+# Generate and display the time travel message
+message = cm.generate_time_travel_message(target_year, selected_destination, final_cost)
+print("\n" + message)
+
--- custom_module.py
+++ custom_module.py
@@ -2,5 +2,16 @@
 
 # Time Travelling Function
 def generate_time_travel_message(year, destination, cost, currency):
-    return f"Prepare for jump! You are travelling to {destination} in the year of {year}. The cost of the trip is {currency}{cost}."
+    """
+    Generates a message prompt for time travelling.
+    Parameters:
+    year(int): The target year for time travel.
+    destination(str): The destination for the time travel.
+    cost(Decimal): The cost of the time travel.
+    currency(str): The currency of the cost.
+    """
+    
+    return f"Prepare for jump! You are travelling to {destination} in the year of {year}. The cost of the trip is {currency}{cost:.2f}."
+
+
 
